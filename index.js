6525c067b9c162dd892ad4bd3a9488195c524891
Jonathan Lassen <jonathan.lassen@gmail.com>
2019-06-18 12:26:49 -0400

    search height

--- index.js
+++ index.js
@@ -7,5 +7,38 @@ console.log(BST);
 BST.insert(3, 3);
 BST.insert(1, 1);
 BST.insert(4, 4);
+BST.insert(6, 6);
+BST.insert(9, 9);
+BST.insert(2, 2);
+BST.insert(5, 5);
+BST.insert(7, 7);
 
 console.log(BST);
+
+
+function tree(t){
+    if(!t){
+        return 0;
+    }
+    return tree(t.left) + t.value + tree(t.right)
+}
+
+console.log(tree(BST))
+
+function height(t, counter = 0){
+    if(!t){
+        return 0;
+    }
+    counter++;
+    let left = counter;
+    let right = counter;
+    if (t.left)
+        left = height(t.left, counter)
+
+    if (t.right)
+        right = height(t.right, counter)
+
+    return left > right ? left: right;
+}
+
+console.log(height(BST))
